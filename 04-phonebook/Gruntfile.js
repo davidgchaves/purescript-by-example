7cd3455d90507d3256f2d5ac0ea955aee1eba226
David G Chaves <davidgchaves@gmail.com>
2014-08-16 17:43:50 +0200

    gruntfile: config grunt-purescript to generate a .psci file

--- 04-phonebook/Gruntfile.js
+++ 04-phonebook/Gruntfile.js
@@ -11,10 +11,11 @@ module.exports = function(grunt) {
         src: ["<%=srcFiles%>"],
         dest: "dist/Main.js"
       }
-    }
+    },
+    dotPsci: ["<%=srcFiles%>"]
   });
 
   grunt.loadNpmTasks("grunt-purescript");
 
-  grunt.registerTask("default", ["psc:all"]);
+  grunt.registerTask("default", ["psc:all", "dotPsci"]);
 };
