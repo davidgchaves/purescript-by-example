b25ab1e490e6966b868bf5417c8f634ea209f1c6
David G Chaves <davidgchaves@gmail.com>
2014-08-16 17:51:57 +0200

    Add showEntry function

--- 04-phonebook/src/Data/PhoneBook.purs
+++ 04-phonebook/src/Data/PhoneBook.purs
@@ -15,3 +15,16 @@ type Entry = { firstName :: String, lastName :: String, phone :: String }
 --  The List Type Constructor takes a Type Argument (Entry) and constructs the new type List Entry
 type PhoneBook = List Entry
 
+
+--
+-- Functions
+--
+
+showEntry :: Entry -> String
+showEntry entry = entry.firstName ++ " " ++ entry.lastName ++ ": " ++ entry.phone
+-- > :i Data.PhoneBook
+-- > let xoan = { firstName: "Xoan", lastName: "Sen Medo", phone: "981-112233" }
+-- > showEntry xoan
+--      Compiling Data.PhoneBook
+--      "Xoan Sen Medo: 981-112233"
+
