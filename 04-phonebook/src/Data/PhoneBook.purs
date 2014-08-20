92bad795f277dbf7cfeb2fa52ecd37c96c6f38df
David G Chaves <davidgchaves@gmail.com>
2014-08-20 21:01:50 +0200

    Add findEntry function

--- 04-phonebook/src/Data/PhoneBook.purs
+++ 04-phonebook/src/Data/PhoneBook.purs
@@ -1,6 +1,7 @@
 module Data.PhoneBook where
 
 import Data.List
+import Data.Maybe
 import Control.Plus (empty)
 
 --
@@ -35,3 +36,8 @@ emptyBook = empty
 insertEntry :: Entry -> PhoneBook -> PhoneBook
 insertEntry = Cons
 
+findEntry :: String -> String -> PhoneBook -> Maybe Entry
+findEntry firstName lastName = head <<< filter filterEntry
+    where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
